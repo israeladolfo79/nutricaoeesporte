593d3acbd8c01c444676c92b3cf48a49c1b2113d
Mateus Moutinho Queiroz <61179749+mateusmoutinho@users.noreply.github.com>
2022-02-04 03:55:11 -0300

    criacao do build

--- build.py
+++ build.py
@@ -3,10 +3,29 @@ from audioop import reverse
 from os import walk,getcwd
 
 
-
-def get_extension(file:str)->str:
-    elemns = file.split('.')
+class Arquivo:
 
 
-for root, dirs, files in walk(getcwd()):
-    print(files)
+    def __init__(self,dir:str,file:str) -> None:
+      
+        self.extension = file.split('.')[-1]
+
+        with open( f'{dir}\{file}','r') as arq:
+            content = arq.read()
+    
+    @staticmethod
+    def get_type(extension:str)->str:
+                
+
+def retorna_arquivos(pasta:str)->list[Arquivo]:
+    arquivos:list[Arquivo] = []
+
+    for dir,root, files in walk(pasta):
+        files.reverse()
+        for file in files:
+            arquivos.append(Arquivo(dir,file))
+    arquivos.reverse()
+    return arquivos
+
+
+
